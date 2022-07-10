f341f1a72a879303ad3a794b7659c4e0f0e000e8
SAURABH SINGH <97328996+Saurabhsinghs@users.noreply.github.com>
2022-07-10 15:20:43 +0530

    Add files via upload

---
+++ src/components/data.js
@@ -0,0 +1,85 @@
+const data= [
+    {
+        questionText: 'Which of the following command is used to create react-js-app ?',
+        point:10,
+        answerOptions: [
+            { answerText: 'npx create-react-app appname', isCorrect:true },
+            { answerText: 'npm install create-react-app', isCorrect: false },
+            { answerText: 'npx install create-react-app -g', isCorrect: false },
+            { answerText: 'install - l create-react-app', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'In React.js which one of the following is used to create a class for Inheritance ?',
+        point:100,
+        answerOptions: [
+            { answerText: 'Create', isCorrect: false },
+            { answerText: 'Extends', isCorrect: true },
+            { answerText: 'Inherits', isCorrect: false },
+            { answerText: 'Delete', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What is the default port number in which the application run ?',
+        point:12,
+        answerOptions: [
+            { answerText: '3000', isCorrect: true },
+            { answerText: '5000', isCorrect: false },
+            { answerText: '5001', isCorrect: false },
+            { answerText: '6000', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which of the following valid component return type of React ?',
+        point:15,
+        answerOptions: [
+            { answerText: '8', isCorrect: false },
+            { answerText: '4', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+            { answerText: '1', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Which of the following is a way to handle data in React.js ?',
+        point:20,
+        answerOptions: [
+            { answerText: 'State & Component', isCorrect: false },
+            { answerText: 'State & Services', isCorrect: false },
+            { answerText: 'Services & Components', isCorrect: false },
+            { answerText: 'State & Props', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Which of the following is must for the API in React.js ?',
+        point:25,
+        answerOptions: [
+            { answerText: 'SetinitialComponent', isCorrect: false },
+            { answerText: 'All of the above', isCorrect: false },
+            { answerText: 'render', isCorrect: false },
+            { answerText: 'renderComponent', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Which of the following is true regarding Babel ?',
+        point:30,
+        answerOptions: [
+            { answerText: 'Compiler', isCorrect: false },
+            { answerText: 'Transpilar', isCorrect: false },
+            { answerText: '	Both of the above', isCorrect: true },
+            { answerText: 'None of the above', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'In React.js, how we can pass the data from one component to another in React.js ?',
+        point:35,
+        answerOptions: [
+            { answerText: 'SetState', isCorrect: false },
+            { answerText: 'Render with arguments', isCorrect: false },
+            { answerText: 'PropTypes', isCorrect: false },
+            { answerText: 'Props', isCorrect: true },
+        ],
+    },
+
+];
+
+export default data
